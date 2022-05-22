d64a29ee83e00c5c4fecaffeeca29a914129f68f
Nina Montana-Brown <nina.brown.15@ucl.ac.uk>
2022-05-22 10:58:26 +0100

    Issue #1: added utilities to calculate final layer size, reverse lists

--- medvae/models/utils.py
+++ medvae/models/utils.py
@@ -4,6 +4,8 @@
 Utility functions for model construction.
 """
 
+import copy
+from math import floor
 from typing import List
 
 def check_input_lists(input_list:List[list]):
@@ -16,3 +18,66 @@ def check_input_lists(input_list:List[list]):
     the_len = len(next(iter_lists))
     if not all(len(l) == the_len for l in iter_lists):
         raise ValueError('Parameter lists not the same length.')
+
+def reverse_lists(input_list:List[list]):
+    """
+    Utility function that reverses
+    the elements in series of lists.
+    :param input_list: List[list]
+    :raise: ValueError if all sublists not same length
+    """
+    check_input_lists(input_list)
+    new_list = []
+    for item in input_list:
+        reverse_list = copy.deepcopy(item)
+        new_list.append(reverse_list.reverse())
+    return new_list
+
+def calculate_final_layer_size(num_filters:int,
+                               input_shape:List[int],
+                               kernel_size:List[int],
+                               stride:List[int],
+                               padding:List[int],
+                               dilation:List[int]=[1,1]):
+    """
+    Function to calculate the final
+    tensor size through a convolutional
+    layer given an input shape and conv
+    parameters.
+    :param num_filters: int
+    :param input_shape: List[int], [Ch, H, W]
+    :param kernel_size: List[int]
+    :param stride: List[int], len(2,)
+    :param padding: List[int], len(2,)
+    :return:
+    """
+    H_out = floor((input_shape[1] + (2*padding[0]) - dilation[0]*(kernel_size[0]-1) -1) / (stride[0])) + 1
+    W_out = floor((input_shape[2] + (2*padding[1]) - dilation[1]*(kernel_size[1]-1) -1) / (stride[1])) + 1
+    return [num_filters, H_out, W_out]
+
+def calculate_size_series_conv(num_filters:List[int],
+                               input_shape:List[list],
+                               kernel_size:List[list],
+                               stride:List[list],
+                               padding:List[list],
+                               dilation:List[list]):
+    """
+    Function to calculate the final
+    tensor size through a convolutional
+    encoder given an input shape and conv
+    parameters.
+    """
+    check_input_lists([num_filters,
+                      kernel_size,
+                      stride,
+                      padding,
+                      dilation])
+    out_shape = input_shape
+    for i, _ in enumerate(kernel_size):
+        out_shape = calculate_final_layer_size(num_filters[i],
+                                               out_shape,
+                                               kernel_size[i],
+                                               stride[i],
+                                               padding[i],
+                                               dilation[i])
+    return out_shape
--- tests/test_models.py
+++ tests/test_models.py
@@ -6,7 +6,7 @@ Testing model construction and calling
 
 import pytest
 import torch
-from medvae.models.utils import check_input_lists
+from medvae.models.utils import calculate_size_series_conv, check_input_lists, calculate_final_layer_size
 from medvae.models import encoder as e
 from medvae.models.ae import AE as V
 from medvae.models.encoder import ConvEncoder as CE
@@ -63,3 +63,49 @@ def test_AE_build(layers_shapes, input_shape):
 def test_ConvEncoder_build(num_filters,input_shape,kernel_size,stride,padding,padding_mode):
     test_CE = CE(num_filters,input_shape,kernel_size,stride,padding,padding_mode)
     output = test_CE.forward(torch.ones(1, *input_shape))
+
+@pytest.mark.parametrize("num_filters,input_shape,kernel_size,stride,padding",
+                         [([3], [1, 30, 30],[2,2],[2,2],[0,0]),
+                          ([10], [1, 100, 100],[5,5],[2,2],[0,0]),
+                          ([12], [1, 20, 20],[3,3],[1,1],[0,0]),
+                          ([3], [1, 5, 5],[1, 2],[3,2],[0,0]),])
+def test_calc_layer_size(num_filters,input_shape,kernel_size,stride,padding,):
+    out_shape = calculate_final_layer_size(num_filters[0], input_shape, kernel_size, stride,padding)
+    conv_layer = torch.nn.Conv2d(1,  num_filters[0],kernel_size,stride,padding,)
+    out_shape_true = conv_layer(torch.ones((1, *input_shape)))
+    assert [1,] + out_shape == list(out_shape_true.shape)
+
+@pytest.mark.parametrize("num_filters,input_shape,kernel_size,stride,padding, dilation",
+                         [([3, 6, 9],
+                           [1, 30, 30],
+                           [[2,2] for i in range(3)],
+                           [[2,2] for i in range(3)],
+                           [[0,0] for i in range(3)],
+                           [[1,1] for i in range(3)]),
+                           ([3, 6, 9, 12],
+                           [1, 100, 100],
+                           [[5,5] for i in range(4)],
+                           [[2,3] for i in range(4)],
+                           [[0,1] for i in range(4)],
+                           [[1,1] for i in range(4)])])
+def test_calc_layer_size_series(num_filters,input_shape,kernel_size,stride,padding,dilation):
+    out_shape = calculate_size_series_conv(num_filters, input_shape, kernel_size, stride,padding, dilation)
+    for i, _ in enumerate(num_filters):
+        if i ==0:
+            conv= torch.nn.Conv2d(input_shape[0],
+                                  num_filters[i],
+                                  kernel_size[i],
+                                  stride[i],
+                                  padding[i],
+                                  dilation[i])
+            out_tens = conv(torch.ones((1, *input_shape)))
+        else:
+            conv = torch.nn.Conv2d(num_filters[i-1],
+                                    num_filters[i],
+                                    kernel_size[i],
+                                    stride[i],
+                                    padding[i],
+                                    dilation[i])
+            out_tens = conv(out_tens)
+    out_shape_true = out_tens.shape
+    assert [1,] + out_shape == list(out_shape_true)
